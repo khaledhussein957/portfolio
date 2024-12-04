99b046571c006c387f74c90cbaf0c88d5888a49d
khaled29-coder <khaledhussein957@gmail.com>
2024-12-04 16:25:37 +0300

    update in the index.js

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,7 @@
 
 import express from "express";
 import dotenv from "dotenv";
-// import cors from "cors";
 import cookieParser from "cookie-parser";
-// import path from "path";
 
 import connectDB from "./src/config/database/mongoDB.js";
 
@@ -16,10 +14,6 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-// const __dirname = path.resolve();
-
-// app.use(cors({ origin: "http://localhost:5173", credentials: true }));
-
 app.use(express.json({ limit: "50mb" })); 
 app.use(cookieParser());
 
@@ -27,13 +21,6 @@ app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/project", projectRoutes);
 
-// if (process.env.NODE_ENV === "production") {
-// 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-// 	app.get("*", (req, res) => {
-// 		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-// 	});
-// }
 
 app.listen(PORT, () => {
 	connectDB();
