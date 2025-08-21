f8530a65df732da4eb31b2f573b17b85b9daa8a1
khaled29-coder <khaledhussein957@gmail.com>
2025-08-21 21:48:25 +0300

    21/8 third commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,17 +3,18 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
-import connectDB from "./src/config/database/mongoDB.js";
+import connectDB from "./config/database/mongoDB.js";
 
-import authRoutes from "./src/routes/authRoutes.js";
-import userRoutes from "./src/routes/userRoutes.js";
-import projectRoutes from "./src/routes/projectRoutes.js";
-import skillRoutes from "./src/routes/skillRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import projectRoutes from "./routes/projectRoutes.js";
+import skillRoutes from "./routes/skillRoutes.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const NODE_ENV = process.env.NODE_ENV;
 
 app.use(
 	cors({
@@ -30,14 +31,10 @@ app.use("/api/v1/user", userRoutes);
 app.use("/api/v1/project", projectRoutes);
 app.use("/api/v1/skill", skillRoutes);
 
-app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`);
-});
-
 const startServer = async () => {
   try {
     await connectDB();
-    if (process.env.NODE_ENV !== "production") {
+    if (NODE_ENV !== "production") {
       app.listen(PORT, () => {
         console.log("Server started on port:", PORT);
       });
