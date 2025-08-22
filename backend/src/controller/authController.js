0052355218ee8ce09d8441c73972462c9fd51ccd
khaled29-coder <khaledhussein957@gmail.com>
2025-08-22 12:17:53 +0300

    22/8 first commit

--- backend/src/controller/authController.js
+++ backend/src/controller/authController.js
@@ -56,7 +56,7 @@ export const signup = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
@@ -136,7 +136,7 @@ export const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
